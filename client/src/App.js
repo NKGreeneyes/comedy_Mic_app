fc346d56642cf5d9b45644ceb3077be8e138c675
Dan Staggs <staggs.dan@gmail.com>
2019-01-28 13:12:38 -0600

    push to catch up whatever I did the other day

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import NoMatch from "./pages/NoMatch";
 import Calendar from "./pages/Calendar";
 import HostHome from "./pages/HostHome";
 import ComedianHome from "./pages/ComedianHome";
+import dotenv from "dotenv";
+dotenv.config();
 
 const App = () => (
   <Router>
