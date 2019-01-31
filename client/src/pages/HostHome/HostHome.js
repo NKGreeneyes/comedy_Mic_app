dae1f454ef88fca027d04177ad75737f685955ea
Victoria <Victoria@Victorias-MacBook-Pro.local>
2019-01-31 14:21:19 -0600

    Created Host Event Form & Submit, edited some CSS,

--- client/src/pages/HostHome/HostHome.js
+++ client/src/pages/HostHome/HostHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import API from "../../utils/API"; //import you API functions here
 import { Col, Row, Container } from "../../components/Grid";
+import CalendarComponent from '../../components/componentCalendar'
 // import Header from "../../components/header.js";
 
 class HostHome extends Component {
@@ -15,8 +16,9 @@ class HostHome extends Component {
           <Col size="md-12">
           {/* <Header text={'dynamic header on the HostHome page'}></Header> */}
             <h1>
-              the host homepage
+              Host Homepage
             </h1>
+            <CalendarComponent />
           </Col>
         </Row>
       </Container>
