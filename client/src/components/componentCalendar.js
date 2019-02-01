de08f4678db88bdcb72078940c2389bd8e0f1f77
Victoria <Victoria@Victorias-MacBook-Pro.local>
2019-02-01 10:21:10 -0600

    updated calendar with backend

--- client/src/components/componentCalendar.js
+++ client/src/components/componentCalendar.js
@@ -9,29 +9,7 @@ const localizer = BigCalendar.momentLocalizer(moment)
 
 const myEventsList=[
 
-    {
-      'title': 'All Day Event',
-      'allDay': true,
-      'start': new Date(2019, 3, 0),
-      'end': new Date(2019, 3, 1)
-    },
-    {
-      'title': 'Long Event',
-      'start': new Date(2019, 3, 7),
-      'end': new Date(2019, 3, 10)
-    },
 
-    {
-      'title': 'DTS STARTS',
-      'start': new Date(2019, 2, 13, 0, 0, 0),
-      'end': new Date(2019, 2, 20, 0, 0, 0)
-    },
-
-    {
-      'title': 'DTS ENDS',
-      'start': new Date(2019, 10, 6, 0, 0, 0),
-      'end': new Date(2019, 10, 13, 0, 0, 0)
-    }
 ]
  
 class CalendarComponent extends React.Component {
